e931115bbdc038cb9975c58f66aa7c8e9a37f723
Santhos <santhoshsunthar@gmail.com>
2023-01-20 11:09:25 +0530

    Added UI

--- ende.py
+++ ende.py
@@ -1,9 +1,13 @@
 import sys
 from PyQt5 import QtGui, QtWidgets
+from main_window import MainWindow
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
+    myapp = MainWindow()
+    myapp.show()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
+
---
+++ main_window.py
@@ -0,0 +1,5 @@
+from PyQt5 import QtGui, QtWidgets
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent = None):
+        super(MainWindow, self).__init__()
